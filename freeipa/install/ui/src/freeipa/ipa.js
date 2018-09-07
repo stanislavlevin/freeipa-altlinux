pvoborni/freeipa

Replace the direct URL with config's one
To be customizable URL should be placed to "config"

Fixes: https://pagure.io/freeipa/issue/7619

install/ui/src/freeipa/ipa.js
@@ -62,7 +62,7 @@ var IPA = function () {
     that.jsonrpc_id = 0;
 
     // live server path
-    that.url = '/ipa/ui/';
+    that.url = config.url;
 
     /**
      * jQuery AJAX options used by RPC commands
@@ -122,7 +122,7 @@ var IPA = function () {
 
         // if current path matches live server path, use live data
         if (that.url && window.location.pathname.substring(0, that.url.length) === that.url) {
-            that.json_url = params.url || '/ipa/session/json';
+            that.json_url = params.url || config.json_url;
 
         } else { // otherwise use fixtures
             that.json_path = params.url || "test/data";
@@ -514,7 +514,7 @@ IPA.login_password = function(username, password) {
     };
 
     var request = {
-        url: '/ipa/session/login_password',
+        url: config.frms_login_url,
         data: data,
         contentType: 'application/x-www-form-urlencoded',
         processData: true,
@@ -587,7 +587,7 @@ IPA.reset_password = function(username, old_password, new_password, otp) {
     }
 
     request = {
-        url: '/ipa/session/change_password',
+        url: config.reset_psw_url,
         data: data,
         contentType: 'application/x-www-form-urlencoded',
         processData: true,

stanislavlevin/freeipa-altlinux

freeipa/install/ui/src/freeipa/ipa.js
@@ -62,7 +62,7 @@ var IPA = function () {
     that.jsonrpc_id = 0;
 
     // live server path
-    that.url = '/ipa/ui/';
+    that.url = config.url;
 
     /**
      * jQuery AJAX options used by RPC commands
@@ -122,7 +122,7 @@ var IPA = function () {
 
         // if current path matches live server path, use live data
         if (that.url && window.location.pathname.substring(0, that.url.length) === that.url) {
-            that.json_url = params.url || '/ipa/session/json';
+            that.json_url = params.url || config.json_url;
 
         } else { // otherwise use fixtures
             that.json_path = params.url || "test/data";
@@ -514,7 +514,7 @@ IPA.login_password = function(username, password) {
     };
 
     var request = {
-        url: '/ipa/session/login_password',
+        url: config.forms_login_url,
         data: data,
         contentType: 'application/x-www-form-urlencoded',
         processData: true,
@@ -587,7 +587,7 @@ IPA.reset_password = function(username, old_password, new_password, otp) {
     }
 
     request = {
-        url: '/ipa/session/change_password',
+        url: config.reset_psw_url,
         data: data,
         contentType: 'application/x-www-form-urlencoded',
         processData: true,