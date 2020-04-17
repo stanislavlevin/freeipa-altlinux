d32b4415717c790a5f00a25f33e96eda7624c03f
Andrey Bychkov <mrdrew@altlinux.org>
2020-04-17 15:53:05 +0300

    Fix links according a new ntp structure
    Add auto-adding ntp dns record after sync time

--- ipaserver/install/server/replicainstall.py
+++ ipaserver/install/server/replicainstall.py
@@ -23,10 +23,10 @@ from pkg_resources import parse_version
 import six
 
 from ipaclient.install.client import check_ldap_conf
-import ipaclient.install.timeconf
 from ipalib.install import certstore, sysrestore
 from ipalib.install.kinit import kinit_keytab
-from ipapython import ipaldap, ipautil
+from ipapython import ipaldap, ipautil, ntpmethods
+from ipapython.ntpmethods import TIME_SERVER
 from ipapython.dn import DN
 from ipapython.admintool import ScriptError
 from ipapython.ipachangeconf import IPAChangeConf
@@ -587,12 +587,12 @@ def common_check(no_ntp):
 
     if not no_ntp:
         try:
-            ipaclient.install.timeconf.check_timedate_services()
-        except ipaclient.install.timeconf.NTPConflictingService as e:
+            ntpmethods.check_timedate_services()
+        except ntpmethods.NTPConflictingService as e:
             print("WARNING: conflicting time&date synchronization service "
-                  "'{svc}' will\nbe disabled in favor of chronyd\n"
-                  .format(svc=e.conflicting_service))
-        except ipaclient.install.timeconf.NTPConfigurationError:
+                  "'{svc}' will\nbe disabled in favor of {ts}\n"
+                  .format(svc=e.conflicting_service, ts=TIME_SERVER))
+        except ntpmethods.NTPConfigurationError:
             pass
 
 
