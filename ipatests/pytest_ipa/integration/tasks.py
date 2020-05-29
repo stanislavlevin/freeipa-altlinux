fb7deed3c84282d8951cd571130df18cd90163f0
Andrey Bychkov <mrdrew@altlinux.org>
2020-05-29 11:58:45 +0300

    Update apt cache before install/remove package

--- ipatests/pytest_ipa/integration/tasks.py
+++ ipatests/pytest_ipa/integration/tasks.py
@@ -2203,6 +2203,7 @@ def install_packages(host, pkgs):
     if platform in ('rhel', 'fedora'):
         install_cmd = ['/usr/bin/dnf', 'install', '-y']
     elif platform in ('ubuntu', 'altlinux'):
+        host.run_command(['apt-get', 'update'])
         install_cmd = ['apt-get', 'install', '-y']
     else:
         raise ValueError('install_packages: unknown platform %s' % platform)
@@ -2243,7 +2244,8 @@ def uninstall_packages(host, pkgs):
     # Only supports RHEL 8+ and Fedora for now
     if platform in ('rhel', 'fedora'):
         install_cmd = ['/usr/bin/dnf', 'remove', '-y']
-    elif platform in ('ubuntu'):
+    elif platform in ('ubuntu', 'altlinux'):
+        host.run_command(['apt-get', 'update'])
         install_cmd = ['apt-get', 'remove', '-y']
     else:
         raise ValueError('install_packages: unknown platform %s' % platform)
